c1d25fc0cba9e4a2534ca0cae6d425263037af01
Maria Pantiushova <dficwriter@gmail.com>
2022-06-18 01:40:48 +0600

    now sends file to server and responds with testing result as dictionary

---
+++ main.js
@@ -0,0 +1,10 @@
+(() => {
+  function _sendFile() {
+    submitForm = document.getElementsByClassName('problem__submit');
+    submitFormFile = document.getElementsByClassName('problem__submit_file');
+    submitFormButton = document.getElementsByClassName('problem__submit_button');
+    // console.log(submitFormButton);
+  }
+
+  _sendFile();
+})();
